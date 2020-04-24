b2b4020e95c03697059e42f1dbc7c3fdeeaf668c
dhafinharish <DHaris454@gmail.com>
2020-04-24 17:59:01 +0800

    Output dengan InputDialog

--- Modul01/DataTypes.java
+++ Modul01/DataTypes.java
@@ -1,7 +1,10 @@
 import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class DataTypes{
  public static void main (String[] args){
+
+  Scanner scanner = new Scanner(System.in);
 
  String namaDepan = "Mirza";
  String namaTengah = "Yogy";
@@ -12,6 +15,19 @@ public class DataTypes{
  char nilaiAbjad = 'A';
  boolean tampan = true;
 
+ System.out.print("Input Nama Depan : ");
+ namaDepan = scanner.nextLine();
+ System.out.println("Input Usia : ");
+ usia = scanner.nextInt();
+ System.out.println("Input IPK: ");
+ ipk = scanner.nextDouble();
+ System.out.println("");
+ System.out.println("Input Nilai Abjad : ");
+ nilaiAbjad = scanner.next().charAt(0);
+ System.out.println("Tampan? : ");
+ tampan = scanner.nextBoolean();
+
+ System.out.println("================OUTPUT=================");
  System.out.println("Nama depan : " + namaDepan);
  System.out.println("Nama tengah : " + namaTengah);
  System.out.println("Nama belakang : " + namaBelakang);
@@ -20,5 +36,7 @@ public class DataTypes{
  System.out.println("IPK : " + ipk);
  System.out.println("Nilai PBO : " + nilaiAbjad);
  System.err.println("Tampan : " + tampan);
+
+ JOptionPane.showMessageDialog(null, "Hai, " + namaDepan + namaTengah + namaBelakang);
  }
 }
